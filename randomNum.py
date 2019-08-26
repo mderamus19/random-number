0befd6ea134d1319b64950141126120593581c2e
Misty DeRamus <mistyderamus@gmail.com>
2019-08-26 15:30:17 -0500

    add boolean to random numbers

--- randomNum.py
+++ randomNum.py
@@ -3,8 +3,13 @@ import random
 my_randoms = list()
 for i in range(10):
     my_randoms.append(random.randrange(1, 6, 1))
-    for number in range(1, 6):
-        if (number in my_randoms):
-            print ("My randoms list contains: ",my_randoms)
-        else:
-            print("My randoms list does not contain: ",number)
+
+for number in range(1, 6):
+    value = False
+    for ran_num in my_randoms:
+        if number == ran_num:
+            value = True
+    if value == True:
+        print ("My randoms list contains: ",number)
+    else:
+        print("My randoms list does not contain: ",number)
